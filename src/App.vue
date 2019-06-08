119ac719cc04838c4bf4d170ea7eb7f361b8ffa3
gsa1270 <hdhd0222@gmail.com>
2019-06-08 12:46:55 +0900

    프로젝트셋팅

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <section class="todoapp">
+      <Header @addTodo="addTodo" /><!--이벤트등록해주기-->
+      <Body :todos="todos" /><!--내려주기-->
+      <Footer/>
+    </section>
+  </div>
+</template>
+
+<script>
+import "./assets/css/main.css";
+
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+export default {
+  components: {
+    Header,
+    Body,
+    Footer
+  },
+  data (){
+    return{
+      todos : []
+    };
+  },
+  methods : {
+    addTodo (text){ /*header에서 받은 text */
+      this.todos = [
+        {
+          id : new Date(),
+          text,
+          isDone : false
+         },
+        ...this.todos
+      ];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input
+      v-model="text"
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+      placeholder="What needs to be done?"
+      @keypress="handleInput" 
+    >
+    <!--keyup , keypress -->
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    handleInput(event) {
+     // console.log(event.keyCode); /*각각의 고유값 enter : 13번 */
+      if(event.keyCode == 13){
+        this.$emit('addTodo' , this.text) //$emit  data 넣어주기
+      }
+      
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+</style>
